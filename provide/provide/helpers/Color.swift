62014d9de21ee06afa7a4192007af92569bc28bd
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2015-05-16 19:18:14 -0400

    Add appearance proxy and static color helper

---
+++ provide/provide/helpers/Color.swift
@@ -0,0 +1,41 @@
+//
+//  Color.swift
+//  provide
+//
+//  Created by Kyle Thomas on 5/16/15.
+//  Copyright (c) 2015 Provide Technologies Inc. All rights reserved.
+//
+
+import UIKit
+
+class Color {
+
+    class func menuBackgroundColor() -> UIColor {
+        return UIColor(red: 0.969, green: 0.965, blue: 0.949, alpha: 1.0)
+    }
+
+    class func darkBlueBackground() -> UIColor {
+        return UIColor(red: 0.008, green: 0.067, blue: 0.231, alpha: 1.00)
+    }
+
+    class func applicationDefaultBackgroundImage() -> UIImage {
+        return UIImage(named: "navbar-background")!
+    }
+
+    class func applicationDefaultBackgroundImageColor(rect: CGRect) -> UIColor {
+        return UIColor.resizedColorWithPatternImage(applicationDefaultBackgroundImage(), rect: rect)
+    }
+
+    class func annotationViewBackgroundImage() -> UIImage {
+        return applicationDefaultBackgroundImage()
+    }
+
+    class func annotationViewBackgroundImageColor() -> UIColor {
+        return UIColor(patternImage: annotationViewBackgroundImage()).colorWithAlphaComponent(0.75)
+    }
+
+    class func annotationViewBackgroundSelectedImageColor() -> UIColor {
+        return UIColor(patternImage: annotationViewBackgroundImage()).colorWithAlphaComponent(1.0)
+    }
+
+}
