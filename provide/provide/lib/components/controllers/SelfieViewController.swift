145396990e9307b3d0a5d4e85f0bdf580d052fa8
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2015-05-16 23:19:38 -0400

    Add selfie view controller component view controller and storyboard

---
+++ provide/provide/lib/components/controllers/SelfieViewController.swift
@@ -0,0 +1,83 @@
+//
+//  SelfieViewController.swift
+//  provide
+//
+//  Created by Kyle Thomas on 5/16/15.
+//  Copyright (c) 2015 Provide Technologies Inc. All rights reserved.
+//
+
+import UIKit
+
+protocol SelfieViewControllerDelegate {
+    func selfieViewController(viewController: SelfieViewController!, didCaptureStillImage image: UIImage!)
+    func selfieViewControllerCanceled(viewController: SelfieViewController!)
+}
+
+class SelfieViewController: ViewController, CameraViewDelegate {
+
+    var delegate: SelfieViewControllerDelegate!
+
+    @IBOutlet private weak var cameraView: CameraView!
+    @IBOutlet private weak var button: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupNavigationItem()
+
+        let swipeGestureRecognizer = UISwipeGestureRecognizer()
+        swipeGestureRecognizer.addTarget(self, action: "dismiss")
+        view.addGestureRecognizer(swipeGestureRecognizer)
+
+        view.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.5)
+
+        setupCameraUI()
+
+        setupCameraView()
+    }
+
+    func setupCameraUI() {
+        view.bringSubviewToFront(button)
+
+        button.addTarget(cameraView, action: "captureFrame", forControlEvents: .TouchUpInside)
+        button.addTarget(self, action: "renderDefaultButtonAppearance", forControlEvents: .TouchUpInside | .TouchUpOutside | .TouchCancel | .TouchDragExit)
+        button.addTarget(self, action: "renderTappedButtonAppearance", forControlEvents: .TouchDown)
+
+        button.addBorder(5.0, color: UIColor.whiteColor())
+        button.makeCircular()
+
+        renderDefaultButtonAppearance()
+    }
+
+    func setupNavigationItem() {
+        navigationItem.title = "TAKE A SELFIE!"
+        navigationItem.hidesBackButton = true
+
+        var cancelItem = UIBarButtonItem(title: "SKIP", style: .Plain, target: self, action: "dismiss")
+        cancelItem.setTitleTextAttributes(AppearenceProxy.barButtonItemTitleTextAttributes(), forState: .Normal)
+
+        navigationItem.leftBarButtonItems = [cancelItem]
+    }
+
+    func setupCameraView() {
+        cameraView?.frame = view.frame
+        cameraView?.delegate = self
+        cameraView?.startCapture()
+    }
+
+    func dismiss() {
+        delegate?.selfieViewControllerCanceled(self)
+    }
+
+    func renderDefaultButtonAppearance() {
+        button.backgroundColor = UIColor.resizedColorWithPatternImage(Color.annotationViewBackgroundImage(), rect: button.bounds).colorWithAlphaComponent(0.75)
+    }
+
+    func renderTappedButtonAppearance() {
+        button.backgroundColor = UIColor.resizedColorWithPatternImage(Color.annotationViewBackgroundImage(), rect: button.bounds)
+    }
+
+    func cameraView(cameraView: CameraView!, didCaptureStillImage image: UIImage!) {
+        delegate?.selfieViewController(self, didCaptureStillImage: image)
+    }
+}
