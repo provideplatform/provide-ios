0114f9ec63a5eda64dbcaaf0a7a8539124056c74
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2015-05-16 23:23:55 -0400

    Add signature component view controller and storyboard

---
+++ provide/provide/lib/components/controllers/SignatureViewController.swift
@@ -0,0 +1,81 @@
+//
+//  SignatureViewController.swift
+//  provide
+//
+//  Created by Kyle Thomas on 5/16/15.
+//  Copyright (c) 2015 Provide Technologies Inc. All rights reserved.
+//
+
+import Foundation
+
+class SignatureViewController: WorkOrderComponentViewController, SignatureViewDelegate {
+
+    @IBOutlet private weak var signatureView: SignatureView!
+    @IBOutlet private weak var summaryLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let transform = CGAffineTransformMakeRotation(CGFloat(M_PI_2));
+        view.transform = transform;
+
+        view.backgroundColor = UIColor.clearColor()
+        view.alpha = 1
+
+        (view.subviews[1] as! UIView).roundCorners(3.0)
+
+        signatureView.delegate = self
+    }
+
+    override func render() {
+        let frame = CGRectMake(0.0,
+            targetView.frame.size.height,
+            targetView.frame.size.width,
+            targetView.frame.size.height)
+
+        view.alpha = 0.0
+        view.frame = frame
+
+        targetView.addSubview(view)
+
+        summaryLabel.text = workOrdersViewControllerDelegate?.summaryLabelTextForSignatureViewController?(self)
+
+        UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveEaseOut,
+            animations: { () -> Void in
+                self.view.alpha = 1
+                self.view.frame = CGRectMake(0.0,
+                                             0.0,
+                                             frame.size.width,
+                                             frame.size.height)
+
+            },
+            completion: { (complete) -> Void in
+                self.suspendTopViewGestureRecognizers()
+            }
+        )
+    }
+
+    override func unwind() {
+        clearNavigationItem()
+
+        UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveEaseOut,
+            animations: { () -> Void in
+                self.view.alpha = 0
+                self.view.frame = CGRectMake(self.view.frame.origin.x,
+                    -self.view.frame.size.height,
+                    self.view.frame.size.width,
+                    self.view.frame.size.height)
+            },
+            completion: { (complete) -> Void in
+                self.enableSuspendedTopViewGestureRecognizers()
+            }
+        )
+    }
+
+    // MARK: SignatureViewDelegate
+
+    func signatureView(signatureView: SignatureView!, capturedSignature signature: UIImage!) {
+        workOrdersViewControllerDelegate?.signatureReceived?(signature, forWorkOrderViewController: self)
+    }
+
+}
---
+++ provide/provide/lib/components/views/SignatureView.swift
@@ -0,0 +1,119 @@
+//
+//  SignaturePanelView.swift
+//  provide
+//
+//  Created by Kyle Thomas on 5/16/15.
+//  Copyright (c) 2015 Provide Technologies Inc. All rights reserved.
+//
+
+import Foundation
+
+@objc
+protocol SignatureViewDelegate {
+
+    func signatureView(signatureView: SignatureView!, capturedSignature signature: UIImage!)
+
+}
+
+class SignatureView: UIView {
+
+    var delegate: SignatureViewDelegate!
+
+    @IBOutlet private weak var doneButton: UIButton!
+
+    private var minTouchesRequired = 20
+    private var path: UIBezierPath!
+    private var writing = false
+    private var touches = 0
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        multipleTouchEnabled = false
+        backgroundColor = UIColor.whiteColor()
+
+        path = UIBezierPath()
+        path.lineWidth = 4.0
+    }
+
+    override func drawRect(rect: CGRect) {
+        UIColor.blackColor().setStroke()
+        path.stroke()
+    }
+
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        writing = true
+
+        if let touch = touches.first as? UITouch {
+            let pt = touch.locationInView(touch.view)
+            path.moveToPoint(pt)
+        }
+
+        write(touches)
+    }
+
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        write(touches)
+    }
+
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        write(touches)
+        writing = false
+    }
+
+    private func write(touches: NSSet!) {
+        if touches == nil {
+            return
+        }
+
+        for touch in touches {
+            var pt = (touch as! UITouch).locationInView(touch.view)
+            path.addLineToPoint(pt)
+
+            self.touches++
+
+            setNeedsDisplay()
+        }
+
+        if self.touches >= minTouchesRequired {
+            enableDoneButton()
+        }
+    }
+
+    private func enableDoneButton() {
+        UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveEaseInOut, animations: { () -> Void in
+            self.doneButton.alpha = 1
+        }) { (finished) -> Void in
+            self.doneButton.enabled = true
+        }
+    }
+
+    @IBAction func doneButtonPressed(sender: UIButton!) {
+        doneButton.enabled = false
+        doneButton.alpha = 0
+        doneButton.removeTarget(nil, action: nil, forControlEvents: .AllEvents)
+
+        if let delegate = self.delegate {
+            delegate.signatureView(self, capturedSignature: image())
+        }
+    }
+
+    func image() -> UIImage! {
+        var img: UIImage!
+
+        let imageSize = bounds.size
+
+        UIGraphicsBeginImageContext(imageSize)
+        var imageContext = UIGraphicsGetCurrentContext()
+
+        //CGContextTranslateCTM(imageContext, 0.0, imageSize.height)
+        //CGContextScaleCTM(imageContext, 1.0, -1.0)
+
+        layer.renderInContext(imageContext)
+        img = UIGraphicsGetImageFromCurrentImageContext()
+
+        UIGraphicsEndImageContext()
+
+        return img
+    }
+}
