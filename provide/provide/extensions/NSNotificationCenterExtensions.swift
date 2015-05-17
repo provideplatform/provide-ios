6bbface3ebfdbc6cc89db12ab90db892ec3f7a60
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2015-05-16 23:41:46 -0400

    Add notification center extensions

---
+++ provide/provide/extensions/NSNotificationCenterExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  NSNotificationCenterExtensions.swift
+//  provide
+//
+//  Created by Kyle Thomas on 5/16/15.
+//  Copyright (c) 2015 Provide Technologies Inc. All rights reserved.
+//
+
+import Foundation
+
+extension NSNotificationCenter {
+
+    func postNotificationName(aName: String) {
+        postNotificationName(aName, object: nil)
+    }
+    
+    func addObserver(observer: AnyObject, selector: Selector, name: String?) {
+        addObserver(observer, selector: selector, name: name, object: nil)
+    }
+    
+    func addObserverForName(name: String?, queue: NSOperationQueue? = nil, usingBlock block: (NSNotification!) -> Void) {
+        addObserverForName(name, object: nil, queue: queue, usingBlock: block)
+    }
+    
+}
