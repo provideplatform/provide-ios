b984634e389e8efa407a035a1217e627e658ed6d
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2015-08-24 22:49:09 -0400

    Revert "Fix warning about variable not being used"
    This reverts commit 4214c3878d0fed1053f518651b4265636575d010.

--- provide/provide/lib/provider/controllers/WorkOrdersViewController.swift
+++ provide/provide/lib/provider/controllers/WorkOrdersViewController.swift
@@ -477,7 +477,7 @@ class WorkOrdersViewController: ViewController, WorkOrdersViewControllerDelegate
 
     func nextWorkOrderContextShouldBeRewound() {
         while managedViewControllers.count > 0 {
-            let _ = popManagedNavigationController()
+            let managedNavigationController = popManagedNavigationController()
             let viewController = managedViewControllers.removeLast()
             unwindManagedViewController(viewController)
         }
