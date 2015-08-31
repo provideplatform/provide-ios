fced020df742918d6ef7bfb48b9da685c874670c
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2015-08-31 05:59:35 -0400

    Add service picker component view controller

---
+++ provide/provide/models/Service.swift
@@ -0,0 +1,24 @@
+//
+//  Service.swift
+//  provide
+//
+//  Created by Kyle Thomas on 5/16/15.
+//  Copyright (c) 2015 Provide Technologies Inc. All rights reserved.
+//
+
+import Foundation
+
+class Service: Model {
+
+    var id: NSNumber!
+    var name: String!
+
+    override class func mapping() -> RKObjectMapping {
+        let mapping = RKObjectMapping(forClass: self)
+        mapping.addAttributeMappingsFromDictionary([
+            "id": "id",
+            "name": "name"
+            ])
+        return mapping
+    }
+}
