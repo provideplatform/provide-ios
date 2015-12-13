41c02a668dd64963f2426a6a3bd67d4f181f2f7b
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2015-12-13 00:52:17 -0500

    Setup provider creation view controller to attempt provider creation when provider object to be created is valid

--- provide/provide/lib/components/controllers/ProviderCreationViewController.swift
+++ provide/provide/lib/components/controllers/ProviderCreationViewController.swift
@@ -17,6 +17,8 @@ class ProviderCreationViewController: UITableViewController, UITextFieldDelegate
     var delegate: ProviderCreationViewControllerDelegate!
 
     @IBOutlet private weak var nameTextField: UITextField!
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var mobileNumberTextField: UITextField!
 
     @IBOutlet private weak var activityIndicatorView: UIActivityIndicatorView!
 
@@ -25,55 +27,84 @@ class ProviderCreationViewController: UITableViewController, UITextFieldDelegate
     }
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if indexPath.section == 1 {
-            let provider = Provider()
-            provider.companyId = 0 // FIXME!! customer.companyId
-            provider.name = nameTextField?.text
+        if indexPath.section == 3 {
+            let user = currentUser()
+            if user.defaultCompanyId > 0 {
+                createProviderWithCompanyId(user.defaultCompanyId)
+            } else {
+                print("WARNING: this user is associated with multiple companies as a provider so cannot attempt creation without user input")
 
-            let providerIsValid = provider.companyId > 0 && provider.name != nil && provider.name.length > 0
-
-            if providerIsValid {
-                for view in tableView.cellForRowAtIndexPath(indexPath)!.contentView.subviews {
-                    if view.isKindOfClass(UIActivityIndicatorView) {
-                        (view as! UIActivityIndicatorView).startAnimating()
-                    } else if view.isKindOfClass(UILabel) {
-                        view.alpha = 0.0
-                    }
-                }
-
-                provider.save(
-                    onSuccess: { statusCode, mappingResult in
-                        if statusCode == 201 {
-                            provider.reload(
-                                onSuccess: { statusCode, mappingResult in
-                                    self.activityIndicatorView?.stopAnimating()
-                                    self.delegate?.providerCreationViewController(self, didCreateProvider: mappingResult.firstObject as! Provider)
-                                },
-                                onError: { error, statusCode, responseString in
-                                    self.activityIndicatorView?.stopAnimating()
-                                }
-                            )
-                        }
-                    },
-                    onError: { error, statusCode, responseString in
-                        self.activityIndicatorView?.stopAnimating()
-                    }
-                )
+//                if user.providers == nil {
+//                    if user.providerIds.count == 1 {
+//                        user.reloadProviders(
+//                            { statusCode, mappingResult in
+//                                if user.providers.count == 1 {
+//                                    self.createProviderWithCompanyId(user.providers.first!.companyId)
+//                                }
+//                            }, onError: { error, statusCode, responseString in
+//
+//                            }
+//                        )
+//                    }
+//                }
             }
         }
 
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 
+    private func createProviderWithCompanyId(companyId: Int) {
+        let provider = Provider()
+        provider.companyId = companyId
+        provider.name = nameTextField?.text
+
+        let contact = Contact()
+        contact.name = nameTextField?.text
+        contact.email = emailTextField?.text
+        contact.mobile = mobileNumberTextField?.text
+        provider.contact = contact
+
+        let providerIsValid = provider.companyId > 0 && provider.name != nil && provider.name.length > 0 && contact.email != nil && contact.email.length > 0
+
+        if providerIsValid {
+            for view in tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0))!.contentView.subviews {
+                if view.isKindOfClass(UIActivityIndicatorView) {
+                    (view as! UIActivityIndicatorView).startAnimating()
+                } else if view.isKindOfClass(UILabel) {
+                    view.alpha = 0.0
+                }
+            }
+
+            provider.save(
+                onSuccess: { statusCode, mappingResult in
+                    if statusCode == 201 {
+                        provider.reload(
+                            onSuccess: { statusCode, mappingResult in
+                                self.activityIndicatorView?.stopAnimating()
+                                self.delegate?.providerCreationViewController(self, didCreateProvider: mappingResult.firstObject as! Provider)
+                            },
+                            onError: { error, statusCode, responseString in
+                                self.activityIndicatorView?.stopAnimating()
+                            }
+                        )
+                    }
+                },
+                onError: { error, statusCode, responseString in
+                    self.activityIndicatorView?.stopAnimating()
+                }
+            )
+        }
+    }
+
     override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
-        if indexPath.section == 2 {
+        if indexPath.section == 3 {
             tableView.cellForRowAtIndexPath(indexPath)!.alpha = 0.8
         }
         return indexPath
     }
 
     override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-        if indexPath.section == 2 {
+        if indexPath.section == 3 {
             tableView.cellForRowAtIndexPath(indexPath)!.alpha = 1.0
         }
     }
