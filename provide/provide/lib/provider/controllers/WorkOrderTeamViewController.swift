c141f2a5aa006bcfd97b6ecda505eb8e66d678f9
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2015-12-18 08:24:30 -0500

    Bump maximumSearchlessProvidersCount in work order team view controller back up to 20

--- provide/provide/lib/provider/controllers/WorkOrderTeamViewController.swift
+++ provide/provide/lib/provider/controllers/WorkOrderTeamViewController.swift
@@ -19,7 +19,7 @@ class WorkOrderTeamViewController: UITableViewController,
                                    ProviderCreationViewControllerDelegate,
                                    DraggableViewGestureRecognizerDelegate {
 
-    let maximumSearchlessProvidersCount = 5
+    let maximumSearchlessProvidersCount = 20
 
     var delegate: WorkOrderTeamViewControllerDelegate! {
         didSet {
