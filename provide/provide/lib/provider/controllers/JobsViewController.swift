289969f2f4a714423e8753aa17562df7a3707e62
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2015-12-29 20:37:57 -0500

    Refactor JobTableViewCell to subclass SWTableViewCell

--- provide/provide/lib/provider/controllers/JobsViewController.swift
+++ provide/provide/lib/provider/controllers/JobsViewController.swift
@@ -13,6 +13,7 @@ class JobsViewController: ViewController,
                           UITableViewDataSource,
                           UIPopoverPresentationControllerDelegate,
                           JobCreationViewControllerDelegate,
+                          JobTableViewCellDelegate,
                           DraggableViewGestureRecognizerDelegate {
 
     @IBOutlet private weak var addJobBarButtonItem: UIBarButtonItem! {
@@ -93,7 +94,6 @@ class JobsViewController: ViewController,
 
             jobCreationViewController = navigationController.viewControllers.first! as! JobCreationViewController
             jobCreationViewController.delegate = self
-
         }
     }
 
@@ -152,74 +152,18 @@ class JobsViewController: ViewController,
         return jobs.count
     }
 
-    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
-    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("jobsTableViewCellReuseIdentifier", forIndexPath: indexPath) as! JobTableViewCell
+        cell.jobTableViewCellDelegate = self
         cell.job = jobs[indexPath.row]
-
-//        if let gestureRecognizers = cell.containerView.gestureRecognizers {
-//            for gestureRecognizer in gestureRecognizers {
-//                if gestureRecognizer.isKindOfClass(JobTableViewCellGestureRecognizer) {
-//                    cell.containerView.removeGestureRecognizer(gestureRecognizer)
-//                }
-//            }
-//        }
-//
-//        let gestureRecognizer = JobTableViewCellGestureRecognizer(viewController: self)
-//        gestureRecognizer.draggableViewGestureRecognizerDelegate = self
-//        cell.containerView.addGestureRecognizer(gestureRecognizer)
-
         return cell
-    }
-
-//    @available(iOS 2.0, *)
-//    optional public func numberOfSectionsInTableView(tableView: UITableView) -> Int // Default is 1 if not implemented
-//
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? // fixed font style. use custom view (UILabel) if you want something different
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String?
-//
-//    // Editing
-//
-//    // Individual rows can opt out of having the -editing property set for them. If not implemented, all rows are assumed to be editable.
-//    @available(iOS 2.0, *)
-
-    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-//
-//    // Moving/reordering
-//
-//    // Allows the reorder accessory view to optionally be shown for a particular row. By default, the reorder control will be shown only if the datasource implements -tableView:moveRowAtIndexPath:toIndexPath:
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool
-//
-//    // Index
-//
-//    @available(iOS 2.0, *)
-//    optional public func sectionIndexTitlesForTableView(tableView: UITableView) -> [String]? // return list of section titles to display in section index view (e.g. "ABCD...Z#")
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, sectionForSectionIndexTitle title: String, atIndex index: Int) -> Int // tell table which section corresponds to section title/index (e.g. "B",1))
-//
-//    // Data manipulation - insert and delete support
-//
-//    // After a row has the minus or plus button invoked (based on the UITableViewCellEditingStyle for the cell), the dataSource must commit the change
-//    // Not called for edit actions using UITableViewRowAction - the action's handler will be invoked instead
-//    @available(iOS 2.0, *)
-    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            cancelJobAtIndexPath(indexPath)
-        }
     }
 
     func cancelJobAtIndexPath(indexPath: NSIndexPath) {
         let job = jobs[indexPath.row]
         if let cell = tableView.cellForRowAtIndexPath(indexPath) as? JobTableViewCell {
             if ["canceled", "completed"].indexOf(job.status) == nil {
-                //cell.dismiss()
+                cell.dismiss()
                 promptForJobCancellation(job, cell: cell)
             }
         }
@@ -251,11 +195,6 @@ class JobsViewController: ViewController,
 
         presentViewController(alertController, animated: true)
     }
-//
-//    // Data manipulation - reorder / moving support
-//
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath)
 
     // MARK: UITableViewDelegate
 
@@ -268,122 +207,11 @@ class JobsViewController: ViewController,
         }
     }
 
-//    @available(iOS 6.0, *)
-//    optional public func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int)
-//    @available(iOS 6.0, *)
-//    optional public func tableView(tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int)
-//    @available(iOS 6.0, *)
-//    optional public func tableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath)
-//    @available(iOS 6.0, *)
-//    optional public func tableView(tableView: UITableView, didEndDisplayingHeaderView view: UIView, forSection section: Int)
-//    @available(iOS 6.0, *)
-//    optional public func tableView(tableView: UITableView, didEndDisplayingFooterView view: UIView, forSection section: Int)
-//
-//    // Variable height support
-//
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat
-//
-//    // Use the estimatedHeight methods to quickly calcuate guessed values which will allow for fast load times of the table.
-//    // If these methods are implemented, the above -tableView:heightForXXX calls will be deferred until views are ready to be displayed, so more expensive logic can be placed there.
-//    @available(iOS 7.0, *)
-//    optional public func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
-//    @available(iOS 7.0, *)
-//    optional public func tableView(tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat
-//    @available(iOS 7.0, *)
-//    optional public func tableView(tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat
-//
-//    // Section header & footer information. Views are preferred over title should you decide to provide both
-//
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? // custom view for header. will be adjusted to default or specified header height
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? // custom view for footer. will be adjusted to default or specified footer height
-//
-//    // Accessories (disclosures).
-//
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath)
-//
-//    // Selection
-//
-//    // -tableView:shouldHighlightRowAtIndexPath: is called when a touch comes down on a row.
-//    // Returning NO to that message halts the selection process and does not cause the currently selected row to lose its selected look while the touch is down.
-//    @available(iOS 6.0, *)
-//    optional public func tableView(tableView: UITableView, shouldHighlightRowAtIndexPath indexPath: NSIndexPath) -> Bool
-//    @available(iOS 6.0, *)
-//    optional public func tableView(tableView: UITableView, didHighlightRowAtIndexPath indexPath: NSIndexPath)
-//    @available(iOS 6.0, *)
-//    optional public func tableView(tableView: UITableView, didUnhighlightRowAtIndexPath indexPath: NSIndexPath)
-//
-//    // Called before the user changes the selection. Return a new indexPath, or nil, to change the proposed selection.
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath?
-//    @available(iOS 3.0, *)
-//    optional public func tableView(tableView: UITableView, willDeselectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath?
-
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        performSegueWithIdentifier("JobWizardTabBarControllerSegue", sender: cell)
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
-
-//    @available(iOS 3.0, *)
-//    optional public func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath)
-//
-//    // Editing
-//
-//    // Allows customization of the editingStyle for a particular cell located at 'indexPath'. If not implemented, all editable cells will have UITableViewCellEditingStyleDelete set for them when the table has editing property set to YES.
-    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
-        return .Delete
-    }
-//    @available(iOS 3.0, *)
-//    optional public func tableView(tableView: UITableView, titleForDeleteConfirmationButtonForRowAtIndexPath indexPath: NSIndexPath) -> String?
-//    @available(iOS 8.0, *)
-//    optional public func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? // supercedes -tableView:titleForDeleteConfirmationButtonForRowAtIndexPath: if return value is non-nil
-//
-//    // Controls whether the background is indented while editing.  If not implemented, the default is YES.  This is unrelated to the indentation level below.  This method only applies to grouped style table views.
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, shouldIndentWhileEditingRowAtIndexPath indexPath: NSIndexPath) -> Bool
-//
-//    // The willBegin/didEnd methods are called whenever the 'editing' property is automatically changed by the table (allowing insert/delete/move). This is done by a swipe activating a single row
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, willBeginEditingRowAtIndexPath indexPath: NSIndexPath)
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, didEndEditingRowAtIndexPath indexPath: NSIndexPath)
-//
-//    // Moving/reordering
-//
-//    // Allows customization of the target row for a particular row as it is being moved/reordered
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, targetIndexPathForMoveFromRowAtIndexPath sourceIndexPath: NSIndexPath, toProposedIndexPath proposedDestinationIndexPath: NSIndexPath) -> NSIndexPath
-//
-//    // Indentation
-//
-//    @available(iOS 2.0, *)
-//    optional public func tableView(tableView: UITableView, indentationLevelForRowAtIndexPath indexPath: NSIndexPath) -> Int // return 'depth' of row for hierarchies
-//
-//    // Copy/Paste.  All three methods must be implemented by the delegate.
-//
-//    @available(iOS 5.0, *)
-//    optional public func tableView(tableView: UITableView, shouldShowMenuForRowAtIndexPath indexPath: NSIndexPath) -> Bool
-//    @available(iOS 5.0, *)
-//    optional public func tableView(tableView: UITableView, canPerformAction action: Selector, forRowAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool
-//    @available(iOS 5.0, *)
-//    optional public func tableView(tableView: UITableView, performAction action: Selector, forRowAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?)
-//
-//    // Focus
-//
-//    @available(iOS 9.0, *)
-//    optional public func tableView(tableView: UITableView, canFocusRowAtIndexPath indexPath: NSIndexPath) -> Bool
-//    @available(iOS 9.0, *)
-//    optional public func tableView(tableView: UITableView, shouldUpdateFocusInContext context: UITableViewFocusUpdateContext) -> Bool
-//    @available(iOS 9.0, *)
-//    optional public func tableView(tableView: UITableView, didUpdateFocusInContext context: UITableViewFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator)
-//    @available(iOS 9.0, *)
-//    optional public func indexPathForPreferredFocusedViewInTableView(tableView: UITableView) -> NSIndexPath?
 
     // MARK: JobCreationViewControllerDelegate
 
@@ -395,9 +223,21 @@ class JobsViewController: ViewController,
         }
 
         // HACK
-        let jobCreationTableViewCell = JobTableViewCell(frame: CGRectZero)
-        jobCreationTableViewCell.job = job
+        let jobCreationTableViewCell = JobTableViewCell(coder: NSCoder())
+        jobCreationTableViewCell!.job = job
         performSegueWithIdentifier("JobWizardTabBarControllerSegue", sender: jobCreationTableViewCell)
+    }
+
+    // MARK: JobTableViewCellDelegate
+
+    func jobTableViewCell(tableViewCell: JobTableViewCell, shouldCancelJob job: Job) {
+        for j in jobs {
+            if j.id == job.id {
+                let indexPath = NSIndexPath(forRow: jobs.indexOfObject(j)!, inSection: 0)
+                cancelJobAtIndexPath(indexPath)
+                return
+            }
+        }
     }
 
     // MARK: DraggableViewGestureRecognizerDelegate
@@ -407,9 +247,6 @@ class JobsViewController: ViewController,
     }
 
     func draggableViewGestureRecognizer(gestureRecognizer: DraggableViewGestureRecognizer, shouldAnimateResetView view: UIView) -> Bool {
-//        if gestureRecognizer.isKindOfClass(JobTableViewCell) {
-//            return (gestureRecognizer as! JobTableViewCellGestureRecognizer).shouldAnimateViewReset
-//        }
         return false
     }
 
@@ -429,14 +266,6 @@ class JobsViewController: ViewController,
 
         private var shouldCancelJob = false
 
-//        private var window: UIWindow! {
-//            return UIApplication.sharedApplication().keyWindow!
-//        }
-
-//        var shouldAnimateViewReset: Bool {
-//            return !shouldCancelJob
-//        }
-
         init(viewController: JobsViewController) {
             super.init(target: viewController, action: "jobsTableViewCellGestureRecognized:")
             jobsViewController = viewController
@@ -446,20 +275,8 @@ class JobsViewController: ViewController,
             didSet {
                 if let initialView = initialView {
                     if initialView.superview!.isKindOfClass(JobTableViewCell) {
-//                        tableView = initialView.superview! as! UITableView
-//                        tableView?.scrollEnabled = false
-
-                        //initialView.frame = tableView.convertRect(initialView.frame, toView: nil)
-
-//                        window.addSubview(initialView)
-//                        window.bringSubviewToFront(initialView)
                     }
                 } else if let _ = oldValue {
-                    //supervisorsPickerCollectionView.backgroundColor = initialSupervisorsPickerCollectionViewBackgroundColor
-
-//                    tableView?.scrollEnabled = true
-//                    tableView = nil
-
                     shouldCancelJob = false
                 }
             }
@@ -530,6 +347,6 @@ class JobsViewController: ViewController,
     }
 
     deinit {
-        print("deinitialized jobs view controller")
+        logInfo("Deinitialized jobs view controller")
     }
 }
--- provide/provide/lib/provider/views/JobTableViewCell.swift
+++ provide/provide/lib/provider/views/JobTableViewCell.swift
@@ -8,20 +8,43 @@
 
 import UIKit
 
-class JobTableViewCell: UITableViewCell {
+protocol JobTableViewCellDelegate {
+    func jobTableViewCell(tableViewCell: JobTableViewCell, shouldCancelJob job: Job)
+}
+
+class JobTableViewCell: SWTableViewCell, SWTableViewCellDelegate {
+
+    var jobTableViewCellDelegate: JobTableViewCellDelegate!
+
+    var job: Job! {
+        didSet {
+            if let _ = job {
+                refresh()
+            }
+        }
+    }
 
     @IBOutlet weak var containerView: UIView!
     @IBOutlet private weak var backgroundContainerView: UIView!
 
     @IBOutlet private weak var nameLabel: UILabel!
 
-    var job: Job! {
-        didSet {
-            if let job = job {
-                nameLabel?.text = job.name
-                nameLabel?.sizeToFit()
-            }
+    private var showsCancelButton: Bool {
+        if job == nil {
+            return false
         }
+        return job.status != "completed" && job.status != "canceled"
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        backgroundColor = UIColor.clearColor()
+        contentView.backgroundColor = UIColor.clearColor()
+
+        delegate = self
+
+        refreshUtilityButtons()
     }
 
     override func prepareForReuse() {
@@ -33,6 +56,10 @@ class JobTableViewCell: UITableViewCell {
     func dismiss() {
         UIView.animateWithDuration(0.3, delay: 0.1, options: .CurveEaseOut,
             animations: { [weak self] in
+                if let superview = self?.rightUtilityButtons.first!.superview {
+                    superview!.backgroundColor = Color.abandonedStatusColor()
+                    superview!.alpha = 1.0
+                }
                 self!.frame.origin.x = self!.frame.size.width * -2.0
             },
             completion: { complete in
@@ -41,19 +68,73 @@ class JobTableViewCell: UITableViewCell {
         )
     }
 
+    func refresh() {
+        refreshUtilityButtons()
+
+        if let job = job {
+            nameLabel?.text = job.name
+            nameLabel?.sizeToFit()
+        }
+    }
+
     func reset() {
         UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveEaseOut,
-            animations: { [weak self] in
-                self!.frame.origin.x = 0.0
-                self!.containerView.frame.origin.x = 0.0
-                self!.containerView.backgroundColor = self!.containerView.backgroundColor?.colorWithAlphaComponent(0.0)
+            animations: {
+                self.frame.origin.x = 0.0
+                self.containerView.frame.origin.x = 0.0
+                if let superview = self.rightUtilityButtons.first!.superview {
+                    superview!.alpha = 1.0
+                }
+                self.hideUtilityButtonsAnimated(true)
             },
-            completion: { [weak self] complete in
-                if self!.selected && self!.highlighted {
-                    self!.containerView.backgroundColor = UIColor.whiteColor()
+            completion: { complete in
+                if self.selected && self.highlighted {
+                    self.containerView.backgroundColor = UIColor.whiteColor()
                 }
             }
         )
 
+    }
+
+    private func refreshUtilityButtons() {
+        let leftUtilityButtons = NSMutableArray()
+        let rightUtilityButtons = NSMutableArray()
+
+        if showsCancelButton {
+            rightUtilityButtons.sw_addUtilityButtonWithColor(Color.abandonedStatusColor(), title: "Cancel")
+        }
+
+        setLeftUtilityButtons(leftUtilityButtons as [AnyObject], withButtonWidth: 0.0)
+        setRightUtilityButtons(rightUtilityButtons as [AnyObject], withButtonWidth: 120.0)
+    }
+
+    // MARK: SWTableViewCellDelegate
+
+    func swipeableTableViewCell(cell: SWTableViewCell!, didTriggerLeftUtilityButtonWithIndex index: Int) {
+        //  no-op
+    }
+
+    func swipeableTableViewCell(cell: SWTableViewCell!, didTriggerRightUtilityButtonWithIndex index: Int) {
+        if index == 0 {
+            if showsCancelButton {
+                jobTableViewCellDelegate?.jobTableViewCell(self, shouldCancelJob: job)
+            }
+        }
+    }
+
+    func swipeableTableViewCell(cell: SWTableViewCell!, canSwipeToState state: SWCellState) -> Bool {
+        return true
+    }
+
+    func swipeableTableViewCellShouldHideUtilityButtonsOnSwipe(cell: SWTableViewCell!) -> Bool {
+        return true
+    }
+
+    func swipeableTableViewCell(cell: SWTableViewCell!, scrollingToState state: SWCellState) {
+        // no-op
+    }
+
+    func swipeableTableViewCellDidEndScrolling(cell: SWTableViewCell!) {
+        // no-op
     }
 }
