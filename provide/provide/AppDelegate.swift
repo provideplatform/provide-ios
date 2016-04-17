97fb47c3a6f4549e1eac3eb3903a0e8fffb62b8c
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2016-04-17 04:39:55 -0400

    Refactor call to RKObjectMapping.setDefaultSourceToDestinationKeyTransformationBlock

--- provide/provide/AppDelegate.swift
+++ provide/provide/AppDelegate.swift
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         RKLogConfigureFromEnvironment()
 
-        RKEntityMapping.setDefaultSourceToDestinationKeyTransformationBlock { objectMapping, keyPath in
+        RKObjectMapping.setDefaultSourceToDestinationKeyTransformationBlock { objectMapping, keyPath in
             return keyPath.snakeCaseToCamelCaseString()
         }
 
