7998db0459dc619f9be27aa4060b66cc8fa00462
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2016-04-29 22:15:19 -0400

    Remove commented code

--- provide/provide/lib/components/controllers/BlueprintViewController.swift
+++ provide/provide/lib/components/controllers/BlueprintViewController.swift
@@ -223,15 +223,6 @@ class BlueprintViewController: WorkOrderComponentViewController,
 
     private var newWorkOrderPending = false
 
-//    override var navigationController: UINavigationController! {
-//        if let navigationController = super.navigationController {
-//            if let parentNavigationController = navigationController.navigationController {
-//                return parentNavigationController
-//            }
-//        }
-//        return super.navigationController
-//    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
