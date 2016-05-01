e8d4cc8f31d4283e8e288dc0cea86b1985f11863
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2016-05-01 07:00:50 -0400

    Remove info log

--- provide/provide/lib/components/controllers/BlueprintsPageViewController.swift
+++ provide/provide/lib/components/controllers/BlueprintsPageViewController.swift
@@ -155,8 +155,6 @@ class BlueprintsPageViewController: UIPageViewController,
         if viewControllers.count > 0 {
             let viewController = viewControllers[selectedIndex]
             setViewControllers([viewController], direction: direction, animated: animated, completion: { complete in
-                logInfo("Blueprint page view controller rendered \(viewControllers.first!)")
-
                 self.pageViewController(self, willTransitionToViewControllers: viewControllers)
             })
         }
