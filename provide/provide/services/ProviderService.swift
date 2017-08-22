29ff793a9ae54948ebb376857ffaf2b60a20b7ac
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2017-08-22 09:40:17 -0400

    Reintroduce previously-removed provider service

---
+++ provide/provide/services/ProviderService.swift
@@ -0,0 +1,59 @@
+//
+//  ProviderService.swift
+//  provide
+//
+//  Created by Kyle Thomas on 5/16/15.
+//  Copyright © 2016 Provide Technologies Inc. All rights reserved.
+//
+import KTSwiftExtensions
+
+typealias OnProvidersFetched = (_ providers: [Provider]) -> ()
+
+class ProviderService: NSObject {
+
+    fileprivate var providers = [Provider]()
+
+    fileprivate static let sharedInstance = ProviderService()
+
+    class func sharedService() -> ProviderService {
+        return sharedInstance
+    }
+    
+    func fetch(  // consider adding a radius param, in miles, configured server-side and fetched after authentication
+        _ page: Int = 1,
+        rpp: Int = 10,
+        standalone: Bool = false,
+        available: Bool = false,
+        active: Bool = false,
+        nearbyCoordinate: CLLocationCoordinate2D!,
+        onProvidersFetched: @escaping OnProvidersFetched)
+    {
+        var params = [
+            "page": page,
+            "rpp": rpp,
+            "standalone": standalone ? "true" : "false",
+            "available": available ? "true" : "false",
+            "active": active ? "true" : "false",
+        ] as [String : Any]
+        
+        if let nearbyCoordinate = nearbyCoordinate {
+            params["nearby"] = "\(nearbyCoordinate.latitude),\(nearbyCoordinate.longitude)"
+        }
+
+        if let defaultCompanyId = ApiService.sharedService().defaultCompanyId {
+            params["company_id"] = defaultCompanyId
+        }
+
+        ApiService.sharedService().fetchProviders(
+            params as [String : AnyObject],
+            onSuccess: { statusCode, mappingResult in
+                let fetchedProviders = mappingResult?.array() as! [Provider]
+                self.providers += fetchedProviders
+                onProvidersFetched(fetchedProviders)
+            },
+            onError: { error, statusCode, responseString in
+                logWarn("Failed to fetch providers; \(statusCode)")
+            }
+        )
+    }
+}
