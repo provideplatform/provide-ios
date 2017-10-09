0ada2dec924449dc3082b23ba9e8d15b28139db8
Jawwad Ahmad <jawwad.f.ahmad@gmail.com>
2017-10-09 06:36:09 +0500

    Implement a way to quickly write out some of the initial XCTAssert cases

--- provide/provideTests/models/Model+Provide.swift
+++ provide/provideTests/models/Model+Provide.swift
@@ -17,6 +17,10 @@ extension Model {
         let data = try! Data(contentsOf: fileURL)
         let dictionary = decodeJSON(data)
 
+
+        // Uncomment when creating a test to get an initial test body
+        // testWriter(dictionary: dictionary)
+
         let mapperOperation = RKMapperOperation(representation: ["object": dictionary], mappingsDictionary: ["object": mapping()])!
         try! mapperOperation.execute()
         return object(from: mapperOperation, type: self)
@@ -25,6 +29,30 @@ extension Model {
     @nonobjc private class func object<T: Model>(from mapperOperation: RKMapperOperation, type: T.Type) -> T {
         return mapperOperation.mappingResult.firstObject as! T
     }
+}
+
+
+private func testWriter(dictionary: [String: Any]) {
+    print("\n" + String(repeating: "🚀", count: 40))
+
+    for key in dictionary.keys.sorted() {
+        let value = dictionary[key]!
+        let camelKey = key.snakeCaseToCamelCaseString()
+        let variableName = "object"
+
+        switch value {
+        case is NSNull:
+            print("XCTAssertEqual(\(variableName).\(camelKey), nil)")
+        case is String:
+            print("XCTAssertEqual(\(variableName).\(camelKey), \"\(value)\")")
+        case is NSNumber:
+            print("XCTAssertEqual(\(variableName).\(camelKey), \(value))")
+        default:
+            print("XCTAssertNotNil(\(variableName).\(camelKey))")
+        }
+    }
+
+    print(String(repeating: "🚀", count: 40) + "\n")
 }
 
 
