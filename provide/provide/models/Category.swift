d6352f59fb813b4413ea64afb1093de936fc3d23
Jawwad Ahmad <jawwad.f.ahmad@gmail.com>
2017-10-19 11:47:40 +0500

    Add Category model and tests

---
+++ provide/provide/models/Category.swift
@@ -0,0 +1,28 @@
+//
+//  Category.swift
+//  provide
+//
+//  Created by Jawwad Ahmad on 10/19/17.
+//  Copyright © 2017 Provide Technologies Inc. All rights reserved.
+//
+
+import Foundation
+import RestKit
+
+@objcMembers
+class Category: Model {
+
+    var id = 0
+    var name = ""
+    var abbreviation = ""
+
+    override class func mapping() -> RKObjectMapping {
+        let mapping = RKObjectMapping(for: self)
+        mapping?.addAttributeMappings(from: [
+            "id",
+            "name",
+            "abbreviation",
+        ])
+        return mapping!
+    }
+}
---
+++ provide/provideTests/models/CategoryTests.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryTests.swift
+//  provideTests
+//
+//  Created by Jawwad Ahmad on 10/19/17.
+//  Copyright © 2017 Provide Technologies Inc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+@testable import provide
+
+class CategoryTests: XCTestCase {
+
+    func testObjectMapping() {
+        let object = Category.from(file: "HTTPStubs/api/categories/category.json")
+        XCTAssertEqual(object.id, 1)
+        XCTAssertEqual(object.name, "Sedan")
+        XCTAssertEqual(object.abbreviation, "prvdX")
+    }
+}
