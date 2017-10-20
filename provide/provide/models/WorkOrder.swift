a3c18a6959bd2641d63ef489d294469df902b064
Jawwad Ahmad <jawwad.f.ahmad@gmail.com>
2017-10-20 15:28:36 +0500

    Move the estimates_by_category object to be within the config object

--- provide/provide/models/WorkOrder.swift
+++ provide/provide/models/WorkOrder.swift
@@ -80,7 +80,7 @@ class WorkOrder: Model {
             "estimated_distance": "estimatedDistance",
             "estimated_duration": "estimatedDuration",
             "estimated_price": "estimatedPrice",
-            "estimates_by_category": "estimatesByCategory",
+            "config.estimates_by_category": "estimatesByCategory",
             "status": "status",
             "provider_rating": "providerRating",
             "expenses_count": "expensesCount",
