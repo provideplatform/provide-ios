271da522f6686be2e51c759f1d0e5d242d092c94
Jawwad Ahmad <jawwad.f.ahmad@gmail.com>
2017-10-22 12:49:01 +0500

    Implement a custom description property for WorkOrder to cut down on logging in the console

---
+++ provide/provide/models/ModelDescriptions.swift
@@ -0,0 +1,16 @@
+//
+//  ModelDescriptions.swift
+//  provide
+//
+//  Created by Jawwad Ahmad on 10/22/17.
+//  Copyright © 2017 Provide Technologies Inc. All rights reserved.
+//
+
+import Foundation
+
+extension WorkOrder {
+    override var description: String {
+        let address = (config?["destination"] as? [String: Any])?["formatted_address"] ?? ""
+        return "🗓 { id: \(id), status: \(status), distance: \(estimatedDistance), address: \(address) }"
+    }
+}
