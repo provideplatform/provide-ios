cf351d9ae48526005ac064b6cff50465c0e05d0f
Jawwad Ahmad <jawwad.f.ahmad@gmail.com>
2017-10-24 17:31:04 +0500

    Add a simple way to prevent logging from a single location

--- provide/provide/helpers/Logmoji.swift
+++ provide/provide/helpers/Logmoji.swift
@@ -8,6 +8,30 @@
 
 import Foundation
 
-func logmoji(_ emoji: Character, _ items: Any...) {
-    print("\(emoji) \(items) \(emoji)")
+
+// **Uncomment** any items that you don't want to log
+let skipList: [Character] = [
+//    "↗️", // request
+//    "✅", // response
+//    "🌎", // location
+//    "🛑", // stopped checkin service
+//    "✴️", // websocket message
+//    "⚛️", // status
+//    "📝", // status
+//    "🐵", // monkey did something
+//    "🚗", // Added provider annotation
+//    "👱", // user did something
+//    "📌", // checkin
+//    "✳️", // started location service updates
+//    "📍", // location resolved
+//    "🚦", // started checkin service
+//    "💰", // tip
+//    "🗺", // map rendered
+]
+
+
+func logmoji(_ emoji: Character, _ item: Any) {
+    if !skipList.contains(emoji) {
+        print(emoji, item, emoji)
+    }
 }
