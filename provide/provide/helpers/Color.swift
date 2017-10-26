abed63f305dbeff4d1a2b49a930d72d17b165e33
Jawwad Ahmad <jawwad.f.ahmad@gmail.com>
2017-10-26 18:10:54 +0500

    Remove unused colors

--- provide/provide/helpers/Color.swift
+++ provide/provide/helpers/Color.swift
@@ -10,20 +10,12 @@ import UIKit
 
 class Color {
 
-    class func menuBackgroundColor() -> UIColor {
-        return darkBlueBackground()
-    }
-
     class func darkBlueBackground() -> UIColor {
         return UIColor("#202838")
     }
 
     class func applicationDefaultBackgroundImage() -> UIImage {
         return UIImage("background")
-    }
-
-    class func applicationDefaultNavigationBarBackgroundImage() -> UIImage {
-        return UIImage("bar-background")
     }
 
     class func applicationDefaultNavigationBarBackgroundColor() -> UIColor {
@@ -42,10 +34,6 @@ class Color {
         return darkBlueBackground()
     }
 
-    class func applicationDefaultBackgroundImageColor(_ rect: CGRect) -> UIColor {
-        return .resizedColorWithPatternImage(applicationDefaultBackgroundImage(), rect: rect)
-    }
-
     class func pinInputControlBoxBorderColor() -> UIColor {
         return UIColor(red: 0.58, green: 0.72, blue: 0.84, alpha: 1.0)
     }
@@ -54,28 +42,12 @@ class Color {
         return applicationDefaultBackgroundImage()
     }
 
-    class func annotationViewBackgroundImageColor() -> UIColor {
-        return UIColor(patternImage: annotationViewBackgroundImage()).withAlphaComponent(0.75)
-    }
-
-    class func annotationViewBackgroundSelectedImageColor() -> UIColor {
-        return UIColor(patternImage: annotationViewBackgroundImage()).withAlphaComponent(1.0)
-    }
-
     class func polylineStrokeColor() -> UIColor {
         return UIColor(red: 0.392, green: 0.706, blue: 0.820, alpha: 1.0)
     }
 
-    class func confirmationGreenBackground() -> UIColor {
-        return UIColor(red: 0.369, green: 0.843, blue: 0.365, alpha: 1.00)
-    }
-
     class func warningBackground() -> UIColor {
         return UIColor(red: 0.737, green: 0.267, blue: 0.192, alpha: 1.00)
-    }
-
-    class func directionIndicatorBorderColor() -> UIColor {
-        return UIColor("#70d1f0")
     }
 
     class func awaitingScheduleStatusColor() -> UIColor {
@@ -87,14 +59,6 @@ class Color {
     }
 
     class func enRouteStatusColor() -> UIColor {
-        return UIColor("#ffc000")
-    }
-
-    class func loadingStatusColor() -> UIColor {
-        return UIColor("#ffc000")
-    }
-
-    class func unloadingStatusColor() -> UIColor {
         return UIColor("#ffc000")
     }
 
