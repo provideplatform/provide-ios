306432e639b777342dcfc1089af013692a5605e1
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2017-11-05 18:34:30 -0500

    Remove trailing whitespace

--- provide/provide/lib/provider/controllers/WorkOrdersViewController.swift
+++ provide/provide/lib/provider/controllers/WorkOrdersViewController.swift
@@ -594,7 +594,6 @@ class WorkOrdersViewController: ViewController, MenuViewControllerDelegate, Work
                 setupMessagesBarButtonItem()
             }
         }
-        
     }
 
     // MARK: DirectionsViewControllerDelegate
