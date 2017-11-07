b23f4b66574dec99d907d4baa017bf0663d7a9e0
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2017-11-07 04:55:03 -0500

    Render user profile image as work order annotation when available

--- provide/provide/lib/provider/views/WorkOrderPinAnnotationView.swift
+++ provide/provide/lib/provider/views/WorkOrderPinAnnotationView.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class WorkOrderPinAnnotationView: MKAnnotationView {
+    weak var workOrder: WorkOrder!
+
+    @objc dynamic var coordinate: CLLocationCoordinate2D
+    @objc dynamic var title: String?
+    @objc dynamic var subtitle: String?
 
     private var rect: CGRect {
         return CGRect(x: 0.0, y: 0.0, width: 50.0, height: 50.0)
@@ -19,16 +24,28 @@ class WorkOrderPinAnnotationView: MKAnnotationView {
         return UIImageView(image: pin.resize(rect))
     }
 
-    init(frame: CGRect) {
+    private var profileImageView: ProfileImageView!
+
+    required init(workOrder: WorkOrder) {
+        coordinate = workOrder.coordinate!
         super.init(annotation: nil, reuseIdentifier: nil)
 
         backgroundColor = .clear
-        addSubview(imageView)
 
-        centerOffset = CGPoint(x: 0, y: (rect.height / 2.0) * -1.0)
+        if workOrder.user == nil {
+            addSubview(imageView)
+            centerOffset = CGPoint(x: 0, y: (rect.height / 2.0) * -1.0)
+        } else if let profileImageUrl = workOrder.user.profileImageUrl {
+            profileImageView = ProfileImageView(frame: rect)
+            profileImageView.setImageWithUrl(profileImageUrl) { [weak self] in
+                self?.profileImageView.makeCircular()
+                self?.profileImageView.alpha = 1.0
+            }
+            addSubview(profileImageView)
+        }
     }
 
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
     }
 }
